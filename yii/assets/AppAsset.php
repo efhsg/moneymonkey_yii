ef861911a15ba87540f05075745c5b64eb8271ef
esg <efhsg@live.nl>
2024-12-07 21:56:31 +0100

    New bootstrap config

--- yii/assets/AppAsset.php
+++ yii/assets/AppAsset.php
@@ -8,25 +8,35 @@
 namespace app\assets;
 
 use yii\web\AssetBundle;
+use yii\helpers\Json;
 
-/**
- * Main application asset bundle.
- *
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
 class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
-    public $css = [
-        'assets/bootstrap/main.css'
-    ];
-    public $js = [
-        'assets/bootstrap/main.js',
-    ];
+    public $css = [];
+    public $js = [];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap5\BootstrapAsset'
+        'yii\bootstrap5\BootstrapAsset',
     ];
+
+    public function init()
+    {
+        parent::init();
+
+        $manifestPath = \Yii::getAlias('@webroot/assets/bootstrap/manifest.json');
+
+        if (file_exists($manifestPath)) {
+            $manifest = Json::decode(file_get_contents($manifestPath));
+
+            if (isset($manifest['main.css'])) {
+                $this->css[] = $manifest['main.css'];
+            }
+            if (isset($manifest['main.js'])) {
+                $this->js[] = $manifest['main.js'];
+            }
+        }
+    }
 }
+
