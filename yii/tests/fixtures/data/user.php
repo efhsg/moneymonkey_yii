1486d479dc5e1d5ac7e27a1fcf9b3fd4e19f60a6
esg <efhsg@live.nl>
2024-12-09 22:13:23 +0100

    More realistic fixture user

--- yii/tests/fixtures/data/user.php
+++ yii/tests/fixtures/data/user.php
@@ -8,7 +8,6 @@ return [
         'password_hash' => Yii::$app->security->generatePasswordHash('admin'),
         'access_token' => '100_access_token',
         'email' => 'admin@example.com',
-        'password_reset_token' => 'FJvf6GvnNSjrGrE8Oxtgu9PKYe_HXFDm_1733759891',
         'created_at' => time(),
         'updated_at' => time(),
     ],
